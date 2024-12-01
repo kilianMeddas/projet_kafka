a04f12c0e79c273308367dc2282c41fcb0333c98
ad.m.na <149951958+aminaaddd@users.noreply.github.com>
2024-12-01 13:08:23 +0100

    Updated

--- stats/statistics.py
+++ stats/statistics.py
@@ -1,15 +1,13 @@
-# stats.py
-
 from connect import connect_to_db
 
-# Total des ventes
+# Total of sales
 def collect_total_revenue():
     db = connect_to_db()
    
     tickets = db['shop']  # la collection
     total_revenue = 0
 
-    # Additioner le 'total' de chaque ticket
+    # Add total of each ticket
     for ticket in tickets.find({}, {'total': 1}):
         total_revenue += ticket.get('total', 0)
 
