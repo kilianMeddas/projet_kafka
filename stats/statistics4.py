38269e2447b389d70f8291757bc9214d018cc23d
ad.m.na <149951958+aminaaddd@users.noreply.github.com>
2024-12-01 13:11:12 +0100

    Updated

--- stats/statistics4.py
+++ stats/statistics4.py
@@ -4,23 +4,22 @@ from collections import defaultdict
 from datetime import datetime
 
 def collect_sales_by_day_and_month():
-    db = connect_to_db()
+    db = connect_to_db() # connection to database 
     tickets = db['shop']
     
-    sales_by_day = defaultdict(int)  # Pour stocker les ventes par jour
-    sales_by_month = defaultdict(int)  # Pour stocker les ventes par mois
-
+    sales_by_day = defaultdict(int)  # for sales by day
+    sales_by_month = defaultdict(int)  # for sales by month
     for ticket in tickets.find():
         ticket_date = ticket.get('date')
         if ticket_date:
             # Format de la date
             date_obj = datetime.strptime(ticket_date, '%Y-%m-%d %H:%M:%S')
             
-            # 1. Ventes par jour
+            # 1. Sales by day
             day = date_obj.strftime('%Y-%m-%d')
             sales_by_day[day] += 1
             
-            # 2. Ventes par mois
+            # 2. Sales by month
             month = date_obj.strftime('%Y-%m')
             sales_by_month[month] += 1
 
