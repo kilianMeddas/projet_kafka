5d7c8a229d276a4f68d4da9a65b0448db6053fa9
ad.m.na <149951958+aminaaddd@users.noreply.github.com>
2024-12-01 13:12:23 +0100

    Updated

--- stats/statistics5.py
+++ stats/statistics5.py
@@ -6,22 +6,22 @@ def revenue_day_month():
     db = connect_to_db()
     tickets = db['shop']
     
-    revenue_by_day = defaultdict(float)  # Pour stocker le revenu par jour
-    revenue_by_month = defaultdict(float)  # Pour stocker le revenu par mois
+    revenue_by_day = defaultdict(float)  # Revenue by day
+    revenue_by_month = defaultdict(float)  # Revenue by month
 
     for ticket in tickets.find():
         ticket_date = ticket.get('date')
-        total_revenue = ticket.get('total', 0)  # Récupérer le revenu total
+        total_revenue = ticket.get('total', 0)  # Collect total revenue
         
         if ticket_date:
             
             date_obj = datetime.strptime(ticket_date, '%Y-%m-%d %H:%M:%S')
             
-            # 1. Revenu par jour
+            # 1. Calculate revenue by day
             day = date_obj.strftime('%Y-%m-%d')
             revenue_by_day[day] += total_revenue
             
-            # 2. Revenu par mois
+            # 2. Calculate revenue by month
             month = date_obj.strftime('%Y-%m')
             revenue_by_month[month] += total_revenue
 
